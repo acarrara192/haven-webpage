aa0e440311cbed550852f22f21583f9c04580a88
acarrara192 <ari192192@gmail.com>
2023-11-21 18:05:23 -0500

    fixed overflow on homepage

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,27 +3,27 @@ import "./home.css"
 
 function Home() {
     return(
-        <div className="home">
-            <div className="overlay">
-                <div className="text-box">
-                    <div className="text-box-content">
-                        <h2> WELCOME TO </h2>
-                        <h1> HAVEN </h1>
-                        
-                        <p> 
-                        An expansive D&D 5e discord server with Adventure League style play, over 50+ roleplay channels, homebrew rulesets for crafting, guilds, pets & mounts and more! 
-                        </p>
-                        <p>
-                            Join the discord server here: 
-                            <a href="https://discord.gg/qdy9c7CMjf" target="_blank" rel="noopener noreferrer"> https://discord.gg/qdy9c7CMjf </a>
-                        </p>
+        <div className="home-wrapper">
+            <div className="home">
+                <div className="overlay">
+                    <div className="text-box">
+                        <div className="text-box-content">
+                            <h2> WELCOME TO </h2>
+                            <h1> HAVEN </h1>
+                            
+                            <p> 
+                            An expansive D&D 5e discord server with Adventure League style play, over 50+ roleplay channels, homebrew rulesets for crafting, guilds, pets & mounts and more! 
+                            </p>
+                            <p>
+                                Join the discord server here: 
+                                <a href="https://discord.gg/qdy9c7CMjf" target="_blank" rel="noopener noreferrer"> https://discord.gg/qdy9c7CMjf </a>
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div className="art-credit wrapper">
-                    <p className="art-credit-home"> Art by Yuji Himukai</p>
-                </div>
-            </div>
-        </div>   
+            </div>   
+            <p className="art-credit-home"> Art by Yuji Himukai</p>
+        </div>
     )
 }
 
