0611632b9fdc2c265183e2ff9becb103540306f1
acarrara192 <ari192192@gmail.com>
2023-11-21 15:44:49 -0500

    first draft of restyled homepage

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,12 +4,14 @@ import "./home.css"
 function Home() {
     return(
         <div className="home">
+            <div className="overlay">
                 <div className="text-box">
                     <div className="text-box-content">
-                        <h1> WELCOME TO HAVEN! </h1>
+                        <h2> WELCOME TO </h2>
+                        <h1> HAVEN </h1>
+                        
                         <p> 
-                        A realm where you can play Dungeons & Dragons, go on epic quests, roleplay with your favorite characters and much more!
-                        Haven is an expansive D&D 5e discord server with Adventure League style play, over 50+ roleplay channels, homebrew rulesets for crafting, guilds, pets & mounts and more! 
+                        An expansive D&D 5e discord server with Adventure League style play, over 50+ roleplay channels, homebrew rulesets for crafting, guilds, pets & mounts and more! 
                         </p>
                         <p>
                             Join the discord server here: 
@@ -17,7 +19,10 @@ function Home() {
                         </p>
                     </div>
                 </div>
-            <p className="art-credit-home"> Art by Yuji Himukai</p>
+                <div className="art-credit wrapper">
+                    <p className="art-credit-home"> Art by Yuji Himukai</p>
+                </div>
+            </div>
         </div>   
     )
 }
